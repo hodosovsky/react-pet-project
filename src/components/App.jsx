664de62c41143eec8c77fe1a8d2d0d90e056d7bd
hodosovsky <107750587+hodosovsky@users.noreply.github.com>
2023-03-29 13:36:02 +0300

    create theme

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,39 @@
+import styled from 'styled-components';
+import { Button } from './Button/Button';
+import { AiFillAndroid, AiFillCar } from 'react-icons/ai';
+import { Box } from './Box/Box';
+
+const Text = styled.p`
+  /* color: ${p => p.theme.colors.primary}; */
+  /* font-family: ${p => p.theme.fonts.monospace}; */
+  /* font-size: ${p => p.theme.fontSizes.xl}; */
+  /* font-weight: ${p => p.theme.fontWeight.normal}; */
+  border: ${p => p.theme.borders.mormal}${p => p.theme.colors.primary};
+  /* padding: ${p => p.theme.space[3]}; */
+`;
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
+    <Box
+      fontFamily="monospace"
+      bg="secondary"
+      color="text"
+      px={5}
+      py={8}
+      height="100%"
+      width="100%"
+      fontSize="xl"
+      display="flex"
+      justifyContent="senter"
+      alignItems="center"
+      as="main"
     >
-      React homework template
-    </div>
+      <Text>homework template</Text>
+      <Button icon={AiFillCar}>Кнопка</Button>
+      <Button>search</Button>
+      <Button icon={AiFillAndroid} type="submit" disabled>
+        LogIn
+      </Button>
+    </Box>
   );
 };
